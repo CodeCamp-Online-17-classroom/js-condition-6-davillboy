e06baaf3e3bc1faaa61be361c1d8831a35dc3c6b
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:33:04 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+
+<script>
+        let input = prompt("Enter a number");
+
+        if (input % 2 === 0) {
+            alert("Even number");
+        } else {
+            alert("Odd number");
+        }
+        
+</script>
